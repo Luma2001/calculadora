e7f0eb1fcd00b3f9320eae9c6dc00a830d563f64
Luma2001 <lumaqui2001@yahoo.com.ar>
2024-05-23 22:41:18 -0300

    Se agregan botones de calculadora científica

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ function clear(e){
         let newletter = letter.slice(0,-1);
         input[0].value = newletter;
 }
+
+/*Funciones matemáticas*/
+function sqrt(){
+    input[0].value = Math.sqrt(input[0].value);
+}
+function cbrt(){
+    input[0].value = Math.cbrt(input[0].value);
+}
+
+
+/*Funciones trigonométricas*/
+function sen(){
+    input[0].value = Math.sin(input[0].value);
+}
+function cos(){
+    input[0].value= Math.cos(input[0].value);
+}
+function tan(){
+    input[0].value= Math.tan(input[0].value);
+}
+
+/*Funciones Algorítmicas*/
+function log(){
+    input[0].value = Math.log10(input[0].value);
+}
+function ln(){
+    input[0].value = Math.log(input[0].value);
+}
